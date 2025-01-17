a545f788fe95b15c0b145d9e7664941e893e95d8
qldmq <ksh13345@naver.com>
2025-01-17 23:58:59 +0900

    섹션3. 스프링 웹 개발 기초 / 완

--- BasicSpring/src/main/java/ksh/BasicSpring/controller/HelloController.java
+++ BasicSpring/src/main/java/ksh/BasicSpring/controller/HelloController.java
@@ -3,6 +3,8 @@ package ksh.BasicSpring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,37 @@ public class HelloController {
     public String hello(Model model) {
     model.addAttribute("data", "hello!!");
     return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {  // json 방식으로 출력됨 : json은 키와 벨류로 이루어진 구조
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
