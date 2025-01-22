f359b86f6cb9e13b4a8dc320d34998ff19dca26c
qldmq <ksh13345@naver.com>
2025-01-22 22:12:18 +0900

    섹션6. 회원 관리 예제 - 웹 MVC 개발 / 완

--- BasicSpring/src/main/java/ksh/BasicSpring/controller/MemberController.java
+++ BasicSpring/src/main/java/ksh/BasicSpring/controller/MemberController.java
@@ -1,17 +1,44 @@
 package ksh.BasicSpring.controller;
 
+import ksh.BasicSpring.domain.Member;
 import ksh.BasicSpring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
 
-    private final MemberService memberService;
+    private MemberService memberService;
 
     @Autowired  // memberService를 스프링이 스프링 컨테이너에 있는 memberService를 가져와 연결해줌
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";    // 홈화면으로 이동
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
